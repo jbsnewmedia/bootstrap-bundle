e5ccb09cebaf674c2ae5486cf80b4c9485a19b9b
jschwind <info@juergen-schwind.de>
2025-11-24 15:43:04 +0100

    Add Symfony bundle structure and service configuration for BootstrapBundle

---
+++ src/Resources/config/services.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $config): void {
+    $services = $config->services()
+        ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+    $services->load('JBSNewMedia\\BootstrapBundle\\Command\\', __DIR__ . '/../../Command')
+        ->private();
+};
